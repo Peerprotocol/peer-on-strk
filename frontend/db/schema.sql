bd4ef1c43a72f111908b628ded794918c0998873
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2025-02-20 03:14:53 +0100

    create notifications db route

--- frontend/db/schema.sql
+++ frontend/db/schema.sql
@@ -26,6 +26,21 @@ CREATE TABLE protocol_data (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE notifications (
+    id SERIAL PRIMARY KEY,
+    user_address VARCHAR NOT NULL,
+    message VARCHAR NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+INSERT INTO notifications (user_address, message)
+VALUES 
+('0x00B785448cC2e3010c60F7c2F68f1Ae34535EdF5921608E987d6AB204d94D243', 'Your lending proposal has been accepted'),
+('0x00B785448cC2e3010c60F7c2F68f1Ae34535EdF5921608E987d6AB204d94D243', 'New borrow proposal available matching your criteria'),
+('0x00B785448cC2e3010c60F7c2F68f1Ae34535EdF5921608E987d6AB204d94D243', 'Your loan is due in 3 days'),
+('0x00B785448cC2e3010c60F7c2F68f1Ae34535EdF5921608E987d6AB204d94D243', 'Payment received: 2.5 STRK from borrower'),
+('0x00B785448cC2e3010c60F7c2F68f1Ae34535EdF5921608E987d6AB204d94D243', 'Congratulations! You''ve completed your first P2P transaction');
+
 INSERT INTO protocol_data 
     (total_borrow, total_lend, total_p2p_deals, total_interest_earned, total_value_locked)
 VALUES 

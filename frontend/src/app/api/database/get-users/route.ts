415807f51d84ef4ac0b492de78b19d2c5ec5fa53
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2025-02-28 14:52:22 +0100

    little fix

--- frontend/src/app/api/database/get-users/route.ts
+++ frontend/src/app/api/database/get-users/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const result = await sql`
     SELECT * FROM users
+    LIMIT ALL
   `;
   return NextResponse.json(result.rows);
 }

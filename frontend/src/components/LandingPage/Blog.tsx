f7be6aededa16d592fe9edefecc8c84c2de169d1
ShamzX0 <posp.petrr@gmail.com>
2025-03-12 13:50:01 +0900

    tidy: clean up

--- frontend/src/components/LandingPage/Blog.tsx
+++ frontend/src/components/LandingPage/Blog.tsx
@@ -13,7 +13,9 @@ const BlogPost: React.FC<BlogPostProps> = ({ title, summary }) => {
   const { isDarkMode } = useContext(DarkModeContext);
 
   return (
-    <div className="rounded-lg overflow-hidden shadow-lg border border-black h-full flex flex-col">
+    <div
+      className={`rounded-lg overflow-hidden shadow-lg border border-black bg-white text-black h-full flex flex-col m-8 md:m-0 ${isDarkMode && 'border-white'}`}
+    >
       <div className="w-full h-48 relative">
         <Image
           src="/images/Deon.png"
@@ -23,18 +25,20 @@ const BlogPost: React.FC<BlogPostProps> = ({ title, summary }) => {
         />
       </div>
       <div className="px-6 py-4 flex-grow flex flex-col h-64">
-        <div className="font-bold text-xl text-black mb-11 h-14">
+        <div className='font-bold text-xl mb-6 md:mb-11 h-14 text-black'>
           {title}
         </div>
         <div className="flex-grow overflow-hidden">
-          <p className={`${isDarkMode ? "text-white" : "text-gray-700"} text-left line-clamp-5`}>
+          <p className='text-black text-left line-clamp-5'>
             {summary}
           </p>
         </div>
       </div>
       <div className="px-6 pb-6 pt-2">
         <Link href="/blog" className="block w-full">
-          <button className="bg-black text-white py-2 px-4 rounded border border-transparent hover:bg-white hover:text-black hover:border-black w-full">
+          <button
+            className='py-2 px-4 rounded border border-transparent bg-black text-white hover:bg-white hover:text-black hover:border-black w-full'
+          >
             See More
           </button>
         </Link>
